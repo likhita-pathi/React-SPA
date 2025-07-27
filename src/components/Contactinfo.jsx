fb6882c7af8c8b3333daacbf338504392e48af81
Likhita Pathi <likhitapathi0@gmail.com>
2025-07-27 12:01:45 +0530

    Building Contact Page

---
+++ src/components/Contactinfo.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Contactinfo = () => {
+  return (
+    <div>
+      <div>Email:liksss</div>
+      <div>phone:9966xxxxx</div>
+      <div>TCS</div>
+    </div>
+  );
+};
+
+export default Contactinfo;
