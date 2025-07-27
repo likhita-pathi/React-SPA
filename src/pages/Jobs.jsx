7a80c07c60d81d60afc8b1b64cb7c6275b927529
Likhita Pathi <likhitapathi0@gmail.com>
2025-07-27 12:02:24 +0530

    Setting up job portal

---
+++ src/pages/Jobs.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link, useLoaderData } from "react-router-dom";
+
+const Jobs = () => {
+  const jobsData = useLoaderData();
+  return (
+    <div className="jobs">
+      {jobsData.map((job) => {
+        return (
+          <Link to={job.id.toString()} key={job.id}>
+            <h4>{job.title}</h4>
+            <h4>{job.location}</h4>
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Jobs;
+export const JobsLoader = async () => {
+  const res = await fetch("http://localhost:5001/jobs");
+  return res.json();
+};
